39520d6a54ab118d8e4151ac45f6f8050ab35654
Rowland Oka <rowlandoka@gmail.com>
2023-11-07 05:28:56 +0100

    react inline styling

---
+++ 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import BodySection from "./BodySection";
+import PropTypes from "prop-types";
+import { StyleSheet, css } from "aphrodite";
+
+class BodySectionWithMarginBottom extends Component {
+  render() {
+    return (
+      <div className={css(styles.bodySectionWithMargin)}>
+        <BodySection {...this.props} />
+      </div>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  bodySectionWithMargin: {
+    marginBottom: "40px",
+  },
+});
+
+BodySectionWithMarginBottom.defaultProps = {
+  children: <React.Fragment />,
+};
+
+BodySectionWithMarginBottom.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+};
+
+export default BodySectionWithMarginBottom;
---
+++ 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { StyleSheet, css } from "aphrodite";
+
+function Login() {
+  return (
+    <React.Fragment>
+      <div className={css(styles["App-body"])}>
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email: </label>
+        <input
+          className={css(styles.input)}
+          type="email"
+          id="email"
+          name="email"
+        />
+        <label htmlFor="password">Password: </label>
+        <input
+          className={css(styles.input)}
+          type="password"
+          id="password"
+          name="password"
+        />
+        <button>OK</button>
+      </div>
+    </React.Fragment>
+  );
+}
+
+const styles = StyleSheet.create({
+  "App-body": {
+    fontSize: "1rem",
+    padding: "2em",
+    height: "45%",
+  },
+  input: {
+    margin: "10px",
+  },
+});
+
+export default Login;
