f9403c9ba99959994341e61e4ac253f039c4eb4b
Rowland Oka <rowlandoka@gmail.com>
2023-11-12 15:55:44 +0100

    test app

--- 0x05-react_state/task_0/dashboard/src/BodySection/BodySection.test.js
+++ 0x05-react_state/task_0/dashboard/src/BodySection/BodySection.test.js
@@ -1,20 +1,23 @@
-// import React from "react";
-// import { shallow } from "enzyme";
-// import BodySection from "./BodySection";
+import React from "react";
+import { shallow } from "enzyme";
+import BodySection from "./BodySection";
+import { StyleSheetTestUtils } from "aphrodite";
 
-// describe("Testing BodySection Component", () => {
-//   it(" checking that shallowing the component should render correctly the children and one h2 element", () => {
-//     const wrapper = shallow(
-//       <BodySection title="test title">
-//         <p>test children node</p>
-//       </BodySection>
-//     );
-//     const h = wrapper.find("h2").text();
-//     const p = wrapper.find("p").text();
-//     expect(h).toEqual("test title");
-//     expect(p).toEqual("test children node");
-//     expect(wrapper.containsAllMatchingElements([h, p])).toEqual(true);
-//   });
-// });
+describe("Testing BodySection Component", () => {
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
 
-BodySection.js;
+  it(" checking that shallowing the component should render correctly the children and one h2 element", () => {
+    const wrapper = shallow(
+      <BodySection title="test title">
+        <p>test children node</p>
+      </BodySection>
+    );
+    const h = wrapper.find("h2").text();
+    const p = wrapper.find("p").text();
+    expect(h).toEqual("test title");
+    expect(p).toEqual("test children node");
+    expect(wrapper.containsAllMatchingElements([h, p])).toEqual(true);
+  });
+});
