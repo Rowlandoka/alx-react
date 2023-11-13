edbd1d3e315b48859439d22d2cb27434025f0d48
Rowland Oka <rowlandoka@gmail.com>
2023-11-13 02:45:13 +0100

    setup login form with controlled component

--- 0x05-react_state/task_1/dashboard/src/Login/Login.js
+++ 0x05-react_state/task_1/dashboard/src/Login/Login.js
@@ -1,44 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, css } from "aphrodite";
 
-function Login() {
-  return (
-    <div className={css(styles.login)}>
-      <p>Login to access the full dashboard</p>
-      <label htmlFor="email">Email:</label>
-      <input type="email" id="email" className={css(styles.loginInput)} />
-      <label htmlFor="password">Password:</label>
-      <input type="password" id="password" className={css(styles.loginInput)} />
-      <button>OK</button>
-    </div>
-  );
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoggedIn: false,
+      email: "",
+      password: "",
+      enableSubmit: false,
+    };
+  }
+
+  handleLoginSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ isLoggedIn: true });
+  };
+
+  handleChangeEmail = (e) => {
+    e.preventDefault();
+    this.setState({ email: e.target.value }, this.handleEnableSubmit);
+  };
+
+  handleChangePassword = (e) => {
+    e.preventDefault();
+    this.setState({ password: e.target.value }, this.handleEnableSubmit);
+  };
+
+  handleEnableSubmit = () => {
+    if (this.state.email !== "" && this.state.password !== "") {
+      this.setState({ enableSubmit: true });
+    } else {
+      this.setState({ enableSubmit: false });
+    }
+  };
+
+  render() {
+    const { email, password } = this.state;
+    return (
+      <React.Fragment>
+        <div className={css(loginStyles.appBody)}>
+          <p>Login to access the full dashboard</p>
+          <form onSubmit={this.handleLoginSubmit}>
+            <label htmlFor="email">Email: </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className={loginStyles.inputs}
+              value={this.state.email}
+              onChange={this.handleChangeEmail}
+            />
+            <label htmlFor="password">Password: </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className={loginStyles.inputs}
+              value={this.state.password}
+              onChange={this.handleChangePassword}
+            />
+            <input
+              type="submit"
+              value="OK"
+              disabled={this.state.enableSubmit ? false : true}
+            />
+          </form>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
-const screenSize = {
-  small: "@media screen and (max-width: 900px)",
-};
-
-const styles = StyleSheet.create({
-  login: {
-    margin: "50px",
-    flexGrow: 1,
-    [screenSize.small]: {
-      marginTop: "10px",
-      marginLeft: 0,
-      marginRight: 0,
-      marginBottom: 0,
+const loginStyles = StyleSheet.create({
+  appBody: {
+    padding: "36px 24px",
+    "@media (max-width: 900px)": {
+      display: "flex",
+      flexDirection: "column",
     },
   },
 
-  loginInput: {
-    marginLeft: "10px",
-    marginRight: "20px",
-    [screenSize.small]: {
-      display: "block",
-      marginLeft: 0,
-      marginTop: "10px",
-      marginBottom: "10px",
-    },
+  inputs: {
+    margin: "0 16px 0 8px",
   },
 });
 
--- 0x05-react_state/task_1/dashboard/src/Login/Login.test.js
+++ 0x05-react_state/task_1/dashboard/src/Login/Login.test.js
@@ -1,28 +1,33 @@
-import { shallow } from "enzyme";
 import React from "react";
 import Login from "./Login";
+import { mount, shallow } from "enzyme";
 import { StyleSheetTestUtils } from "aphrodite";
 
-describe("<Login />", () => {
-  beforeAll(() => {
+describe("testing the <Login /> component", () => {
+  let wrapper;
+
+  beforeEach(() => {
     StyleSheetTestUtils.suppressStyleInjection();
-  });
-  afterAll(() => {
-    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+    wrapper = shallow(<Login />);
   });
 
-  it("Login renders without crashing", () => {
-    const wrapper = shallow(<Login />);
-    expect(wrapper.exists()).toEqual(true);
+  it("Login component renders without crashing", () => {
+    expect(wrapper).toBeDefined();
   });
-  it("Verify that the components render 2 input", () => {
-    const wrapper = shallow(<Login />);
-    wrapper.update();
-    expect(wrapper.find("div input")).toHaveLength(2);
+
+  it("Login component renders 2 input tags and 2 label tags", () => {
+    expect(wrapper.find("input")).toHaveLength(3);
+    expect(wrapper.find("label")).toHaveLength(2);
   });
-  it("Verify that the components render 2 label", () => {
-    const wrapper = shallow(<Login />);
-    wrapper.update();
-    expect(wrapper.find("div label")).toHaveLength(2);
+});
+
+describe("Test <Login/> with states", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(<Login />);
+  });
+  it("Verify that the submit button is disabled by default", () => {
+    expect(wrapper.state().enableSubmit).toBe(false);
   });
 });
